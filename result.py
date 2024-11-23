fa2f9c0a292b49ffdd820b257c3d0e6cb5750aa7
Limbo-77 <guoqi772100@outlook.com>
2024-11-23 19:35:11 +0800

    Add files via upload

---
+++ result.py
@@ -0,0 +1,69 @@
+[image]  loaded 'workplace.jpg'  (612x407, 3 channels)
+-- ClassID: 1
+-- Confidence: 0.960938
+-- Left: 75.939697
+-- Top: 93.800781
+-- Right: 213.811523
+-- Bottom: 338.239258
+-- Width: 137.871826
+-- Height: 244.438477
+-- Area: 33701.1796875
+-- Center: (144.875610, 216.020020)
+
+-- ClassID: 67
+-- Confidence: 0.590820
+-- Left: 50.427246
+-- Top: 191.476807
+-- Right: 238.166016
+-- Bottom: 349.368164
+-- Width: 187.738770
+-- Height: 157.891357
+-- Area: 29642.330078125
+-- Center: (144.296631, 270.422485)
+
+-- ClassID: 2
+-- Confidence: 0.781738
+-- Left: 432.703125
+-- Top: 178.062500
+-- Right: 597.058594
+-- Bottom: 336.251953
+-- Width: 164.355469
+-- Height: 158.189453
+-- Area: 25999.30078125
+-- Center: (514.880859, 257.157227)
+
+-- ClassID: 1
+-- Confidence: 0.965332
+-- Left: 418.359375
+-- Top: 96.086182
+-- Right: 524.742188
+-- Bottom: 309.224609
+-- Width: 106.382812
+-- Height: 213.138428
+-- Area: 22674.265625
+-- Center: (471.550781, 202.655396)
+
+-- ClassID: 62
+-- Confidence: 0.622559
+-- Left: 71.457275
+-- Top: 195.948242
+-- Right: 212.466797
+-- Bottom: 354.137695
+-- Width: 141.009521
+-- Height: 158.189453
+-- Area: 22306.21875
+-- Center: (141.962036, 275.042969)
+
+-- ClassID: 1
+-- Confidence: 0.834961
+-- Left: 326.917969
+-- Top: 103.339844
+-- Right: 421.347656
+-- Bottom: 325.123047
+-- Width: 94.429688
+-- Height: 221.783203
+-- Area: 20942.91796875
+-- Center: (374.132812, 214.231445)
+
+[image]  saved 'output_image.jpg'  (612x407, 3 channels)
+
